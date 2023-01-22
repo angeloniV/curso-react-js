bfe7dc07c80b2e0c78a3537e1932f00206b3e148
Valentin <valentinangeloni@gmail.com>
2023-01-22 13:45:21 -0300

    Add links and ItemListcontainer border

--- proyecto-preentrega-1/src/components/ItemListContainer.jsx
+++ proyecto-preentrega-1/src/components/ItemListContainer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Text } from '@chakra-ui/react'
+import { Flex, Text, Container } from '@chakra-ui/react'
 
 const ItemListContainer = ({greeting}) => {
     return (
-        <Text fontSize='25px' color='tomato'>{greeting}</Text>
+        <Container maxW="100rem" border="solid" borderColor="yellow.500" borderRadius="5px" maxH="30rem" height="30rem" centerContent>
+            <Flex alignContent="center" gap="2">
+                <Text fontSize='25px' color='tomato'>{greeting}</Text>
+            </Flex>
+        </Container>
     )
 }
 
--- proyecto-preentrega-1/src/components/NavBar.jsx
+++ proyecto-preentrega-1/src/components/NavBar.jsx
@@ -8,16 +8,19 @@ import { Link } from '@chakra-ui/react'
 const NavBar = () => {
     return (
         <>
-            <Container maxW="100rem" backgroundColor="yellow.500" color="#262626">
+            <Container maxW="100rem" backgroundColor="yellow.500" color="#262626" borderRadius="5px">
                 <Flex alignContent="center" gap="2">
                     <Box p="2" color="black">
                         <Heading size="md">E-Commerce</Heading>
                     </Box>
                     <Spacer/>
-                    <Box p="2" color="black">
-                        <Link>Chakra UI</Link>
-                        <Link>Chakra UI</Link>
-                        <Link>Chakra UI</Link>
+                    <Box p="2" color="black" justifyContent={'space-between'}>
+                        <Link p="2">Monitores</Link>
+                        <Link p="2">Notebooks</Link>
+                        <Link p="2">Celulares</Link>
+                        <Link p="2">Gps</Link>
+                        <Link p="2">Memorias</Link>
+                        <Link p="2">Placas de video</Link>
                     </Box>
                     <Spacer/>
                     <Box p="2" color="black">
